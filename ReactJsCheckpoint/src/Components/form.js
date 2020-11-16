a2353e22f1f54dc5463f7c1a74a940cdf971a5d7
Nesrine Ezzine <ezzine.nesrine007@gmail.com>
2020-11-16 10:59:16 +0100

    jsxcheckpoint

--- ReactJsCheckpoint/src/Components/form.js
+++ ReactJsCheckpoint/src/Components/form.js
@@ -1,5 +1,4 @@
 import {Button} from 'react-bootstrap'
-import '../App.css'
 
 function Form() {
     return (
--- jsxcheckpoint/src/code.js
+++ jsxcheckpoint/src/code.js
@@ -1,3 +1,13 @@
-import react from 'react'
 
-export default './App'
+function Code () {
+return (<div>
+    <h1 className="title red">Your name here</h1>
+    <br/>
+    <img src="/imageInSrc.jpg" alt=""/>
+    <br/>
+    <img src="/imageInPublic.jpg" alt=""/>
+    <video width="320" height="240" controls src="myVideo.mp4" type="video/mp4">
+    </video>
+    </div>);}
+
+export default Code
